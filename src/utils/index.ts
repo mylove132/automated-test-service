f132cdc382df65a234a854163b067021d81bfa8c
tower1189 <tower1189@163.com>
2020-01-20 12:34:33 +0800

    feat:新增通过断言获取value方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -20,3 +20,26 @@ export function getRequestMethodTypeString(type: number): Method {
 export function generateEndpointByEnv(env: string, endpoint: string): string {
 	return endpoint
 }
+
+/**
+ * 获取断言的值
+ * @param {any, string} 接口返回值， 断言逗号分割的key
+ */
+export function getAssertObjectValue(obj: any, keys: string): any {
+	// 断言的key列表
+	let keyList = keys.split('.');
+	keyList.shift();
+	const haveArray = new RegExp('\[[0-9]+\]');
+	return keyList.reduce((preValue, currentValue, currentIndex) => {
+		const currentObj = currentIndex == 0 ? obj : preValue;
+		// 含有数组的情况
+		if (haveArray.test(currentValue)) {
+			const temp = haveArray.exec(currentValue).toString();
+			const arrIndex = temp.slice(1, temp.length -1);
+			const key = currentValue.slice(0, currentValue.indexOf(temp));
+			return currentObj[key][arrIndex];
+		} else {
+			return currentObj[currentValue];
+		}
+	}, null)
+}
