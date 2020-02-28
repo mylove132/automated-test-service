db382cc416ffb1e188ab63a71c220b5ce3bb5d10
刘占会 <liuzhanhui@okay.cn>
2020-02-28 12:04:47 +0800

    目录列表增加platform code字段

--- src/api/jmeter/jmeter.controller.ts
+++ src/api/jmeter/jmeter.controller.ts
@@ -2,8 +2,7 @@ import { InjectQueue } from '@nestjs/bull';
 import {Body, Controller, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
 import { Queue } from 'bull';
 import {ApiBearerAuth, ApiUseTags} from '@nestjs/swagger';
-import {FileInterceptor} from "@nestjs/platform-express";
-import {File} from "@babel/types";
+
 
 
 @ApiBearerAuth()
@@ -16,10 +15,4 @@ export class JmeterController {
   async exec_jmeter(@Body() caseIds: number[]) {
     await this.jmeterQueue.add('exec_jmeter', caseIds);
   }
-
-    @Post('upload')
-    @UseInterceptors(FileInterceptor('file'))
-    uploadFile(@UploadedFile() file) {
-      console.log(file.buffer)
-    }
 }
