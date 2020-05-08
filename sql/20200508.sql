d76d7b2a6b286933e713c9126e697f5a004c5c59
刘占会 <liuzhanhui1@xdf.cn>
2020-05-08 19:07:40 +0800

    修改断言为空的表字段

--- sql/20200508.sql
+++ sql/20200508.sql
@@ -1,2 +1,4 @@
 ALTER TABLE "case" DROP CONSTRAINT "FK_7bedaea7848005f61a37b1fe254";
 ALTER TABLE "case" DROP COLUMN "jmeterId";
+ALTER TABLE "case" ALTER COLUMN "assertText" DROP NOT NULL;
+COMMENT ON COLUMN "case"."assertText" IS '断言内容';
